4515812581846ae7dd24f082675a944eb309e129
Jason Justice <jjustice@git.viasat.com>
2017-03-23 14:33:02 -0700

    removed en-us and en-gb

--- src/excuses.js
+++ src/excuses.js
@@ -12,42 +12,13 @@ module.exports = {
                 "I'm having furniture delivered to my house.",
                 "I have to sign for a package.",
                 "A pipe burst this morning, waiting on a plumber.",
+                "The package never came, so I have to wait for it again.",
             ],
             "SKILL_NAME" : "Work From Home Excuses",
             "GET_EXCUSE_MESSAGE" : "Here's your Excuse: ",
-            "HELP_MESSAGE" : "You can say, give me a Excuse, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say, give me an Excuse, or I'm feeling lazy, or you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
-        }
-    },
-    "en-US": {
-        "translation": {
-          "EXCUSES": [
-              "Doctors appointment.",
-              "Sick Kid.",
-              "Food Poisoning.",
-              "Dentist appointment.",
-              "Just spent the past couple of hours in the bathroom, don't ask.",
-              "I'm having furniture delivered to my house.",
-              "I have to sign for a package.",
-              "A pipe burst this morning, waiting on a plumber.",
-          ],
-            "SKILL_NAME" : "American Work From Home EXCUSES"
-        }
-    },
-    "en-GB": {
-        "translation": {
-          "EXCUSES": [
-              "Doctors appointment.",
-              "Sick Kid.",
-              "Food Poisoning.",
-              "Dentist appointment.",
-              "Just spent the past couple of hours in the bathroom, don't ask.",
-              "I'm having furniture delivered to my house.",
-              "I have to sign for a package.",
-              "A pipe burst this morning, waiting on a plumber.",
-          ],
-            "SKILL_NAME" : "British Work From Home EXCUSES"
         }
     },
 };
